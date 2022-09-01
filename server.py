470840a30b04511812a9a0fcec9a636c9afcf498
adibahussain19 <cu.divhacks@gmail.com>
2022-09-01 01:58:34 -0400

    updated ports

--- server.py
+++ server.py
@@ -4,29 +4,30 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 5000))
+
 # client = MongoClient('localhost', 27017)
 
 # db = client.flask_db
 # todos = db.todos
 
 # ROUTES
-@app.route('/', methods=("GET"))
+@app.route('/')
 def welcome():
     return render_template('index.html')
     # return render_template('navbar.html')
 
-@app.route('/about', methods=("GET"))
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/programs', methods=("GET"))
+@app.route('/programs')
 def programs():
     return render_template('programs.html')
 
-@app.route('/highlights', methods=("GET"))
+@app.route('/highlights')
 def highlights():
     return render_template('highlights.html')
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
